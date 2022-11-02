b58f95f57c8a6ecd990d1328fea0fff8db2b6487
datoxx <davit.ardzenadze.05@gmail.com>
2022-11-02 23:59:13 +0400

    add ssl, add drupal user token functional

--- node-server/index.ts
+++ node-server/index.ts
@@ -1,16 +1,22 @@
-import express from 'express';
+import dotenv from 'dotenv';
+import https  from 'https';
+import fs from 'fs';
 import cors from 'cors';
+import express from 'express';
 const mongoose = require('mongoose');
 
 import elecion from './routes/election';
+import user from './routes/user';
+
+dotenv.config();
 
 const app = express();
+app.use(express.urlencoded({extended: true}));
 app.use(cors());
 app.use(express.json());
 
-const PORT = 8888;
 
-const MONGODB_URI = 'mongodb+srv://datoxx:datoxx123@fullstack.sqve7.mongodb.net/girchi-election?retryWrites=true&w=majority';
+const MONGODB_URI = process.env.DB_URI;
 console.log('connecting to', MONGODB_URI)
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -23,8 +29,15 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true 
 
 
 app.use('/', elecion);
+app.use('/', user);
 
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 8888;
+if(process.env.ENVIROMENT === 'local'){
+    https.createServer({
+        key: fs.readFileSync(<string>process.env.SSL_KEY),
+        cert: fs.readFileSync(<string>process.env.SSL_CERT),
+    }, app).listen(PORT, () => {console.log(`Server listening on ${PORT}`)});
+} else {
+    app.listen(PORT, () => {console.log(`Server listening on ${PORT}`)});
+}
---
+++ node-server/routes/user.ts
@@ -0,0 +1,48 @@
+import dotenv from 'dotenv';
+import axios from 'axios';
+import express from 'express';
+dotenv.config();
+
+
+const router = express.Router();
+
+const axiosInstance = axios.create({ baseURL: process.env.DRUPAL_DOMAIN });
+axiosInstance.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+
+router.post("/api", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const refresh_token = req.body.refresh_token;
+  
+    const form = new URLSearchParams();
+    form.append('client_id', <string>process.env.CLIENT_ID);
+    form.append('client_secret',<string>process.env.SECRET_KEY);
+    
+    if(username && password) {
+        form.append('grant_type', 'password');
+        form.append('username', username);
+        form.append('password', password);
+        axiosInstance.post('/oauth/token', form)
+            .then(response => {
+                res.json(response.data);
+            })
+            .catch(error => {
+                res.json({"error": 'error retrieving token'});
+            })
+    }
+  
+    if(refresh_token) {
+        form.append('grant_type', 'refresh_token');
+        form.append('refresh_token', refresh_token);
+        axiosInstance.post('/oauth/token', form)
+            .then(response => {
+                res.json(response.data);
+            })
+            .catch(error => {
+                res.json({"error": 'error refreshing token'});
+            })
+    }
+  })
+
+
+  export default router;
